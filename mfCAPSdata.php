0cc58d65572d5dd7e8f2d1878b950fed29ffb5f5
Justin Husted <jushusted@gmail.com>
2013-03-12 19:43:10 -0300

    Update mfCAPSdata.php

--- mfCAPSdata.php
+++ mfCAPSdata.php
@@ -8,7 +8,7 @@
   <div>
     <?php 
     //$capsPage = readfile($_GET['http://api.fool.com/caps/ws/Ticker/GOOG?apikey=ZbhGBKUXVluPZXywfF7yytztBknJdQWr']);
-	  $capsPage = file_get_contents('http://api.fool.com/caps/ws/Ticker/GOOG?apikey=ZbhGBKUXVluPZXywfF7yytztBknJdQWr')
+	  $capsPage = file_get_contents('http://api.fool.com/caps/ws/Ticker/GOOG?apikey=ZbhGBKUXVluPZXywfF7yytztBknJdQWr');
 	  echo isset($capsPage); ?>
   </div>
 </body>
