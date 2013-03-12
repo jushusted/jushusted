00fa983eeef0167cdadc23e2741cc53e8d2f0938
Justin <Husted>
2013-03-12 18:12:15 -0400

    Add mfCAPSdata.php to read from the MF CAPS API

---
+++ mfCAPSdata.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>JS Bin</title>
+</head>
+<body>
+  <div>
+    <?php $capsPage = readfile($_GET['http://api.fool.com/caps/ws/Ticker/GOOG?apikey=ZbhGBKUXVluPZXywfF7yytztBknJdQWr']); ?>
+	  echo $capsPage;
+  </div>
+	<div>
+	Test
+	</div>
+</body>
+</html>
